ddeb395bcaa585b31a5379984dd3c85c4a2e9393
Sagcyber <immortelleame@gmail.com>
2026-01-02 11:46:07 +0700

    Add Task model, service and POST/GET endpoints

--- src/main/java/org/example/taskmanager/controller/TaskController.java
+++ src/main/java/org/example/taskmanager/controller/TaskController.java
@@ -3,6 +3,8 @@ package org.example.taskmanager.controller;
 import org.example.taskmanager.model.Task;
 import org.example.taskmanager.service.TaskService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +21,10 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
+    }
+    
+    @PostMapping("/tasks")
+    public void addTask(@RequestBody Task task) {
+        taskService.addTask(task);
     }
 }
--- src/main/java/org/example/taskmanager/model/Task.java
+++ src/main/java/org/example/taskmanager/model/Task.java
@@ -6,4 +6,33 @@ public class Task {
     private String taskName;
     private TaskStatus status;
     private LocalDate deadline;
+    
+    public Task(){
+    
+    }
+    
+    public String getTaskName() {
+        return taskName;
+    }
+    
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    
+    public TaskStatus getStatus() {
+        return status;
+    }
+    
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+    
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+    
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+    
 }
