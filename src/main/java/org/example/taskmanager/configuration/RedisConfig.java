2fd1dc76dac5079c7ee3b77dcf6efd63914b25ec
Sagcyber <immortelleame@gmail.com>
2026-01-06 17:13:09 +0700

    feat: implement working GET /tasks/status with Redis caching

--- src/main/java/org/example/taskmanager/configuration/RedisConfig.java
+++ src/main/java/org/example/taskmanager/configuration/RedisConfig.java
@@ -1,11 +1,15 @@
 package org.example.taskmanager.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -15,10 +19,19 @@ public class RedisConfig {
     
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                                                                .entryTtl(Duration.ofMinutes(10))
+                                                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+        
         return RedisCacheManager.builder(factory)
-                       .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
-                                              .entryTtl(Duration.ofMinutes(10))
-                       )
-                       .build();
+                                .cacheDefaults(config)
+                                .build();
     }
 }
+
